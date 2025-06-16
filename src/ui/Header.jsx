88f2f76a689ab65414935380bec5c199d739f2cf
Saodat <saodat.riskiyeva@gmail.com>
2025-06-15 22:16:25 -0400

    building the app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Name</p>
+    </div>
+  );
+}
+
+export default Header;
