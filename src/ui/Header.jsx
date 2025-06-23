5915dcad9fbede78663d8e8fd419f7f86357d294
Saodat <saodat.riskiyeva@gmail.com>
2025-06-23 16:18:10 -0400

    working with colors

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
-    <div>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
+
       <SearchOrder />
+
       <p>Name</p>
-    </div>
+    </header>
   );
 }
 
