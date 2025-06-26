2c1b5ccf8c7e86a34ad70a3e3a2a50f0a5e4d75d
Saodat <saodat.riskiyeva@gmail.com>
2025-06-26 17:46:28 -0400

    configuring tailwind custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      pizza: 'Roboto Mono, monospsace',
+    },
+    extend: {
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
